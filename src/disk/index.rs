340e05de4aa9ae979f22905256d6205893f2e252
Abhik Jain <abhikjain360@gmail.com>
2021-06-08 16:11:58 +0530

    disk/segment.rs completed
    Signed-off-by: Abhik Jain <abhikjain360@gmail.com>

--- src/disk/index.rs
+++ src/disk/index.rs
@@ -9,14 +9,19 @@ use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 /// Size of entry, in bytes.
 pub const ENTRY_SIZE: u64 = 8;
 
-/// Wrapper around index files for convenient reading of bytes sizes.
+/// Wrapper around a index file for convenient reading of bytes sizes.
 ///
 /// Does **not** check any of the constraint enforced by user, or that the index being read from/
 /// written to is valid. Simply performs what asked.
+///
+/// #### Note
+/// It is the duty of the handler of this struct to ensure index file's size does not exceed the
+/// specified limit.
 pub(super) struct Index(File);
 
 impl Index {
-    /// Open a new index file, or create one if doesn't exist.
+    /// Open/create a new index file.
+    #[inline]
     pub(super) fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
         Ok(Self(
             OpenOptions::new()
@@ -28,17 +33,14 @@ impl Index {
     }
 
     /// Get the size of packet at the given index, using the index file.
+    #[inline]
     pub(super) fn read(&mut self, index: u64) -> io::Result<u64> {
         self.0.seek(SeekFrom::Start(index * ENTRY_SIZE))?;
         self.0.read_u64::<BigEndian>()
     }
 
-    /// Append a new value to the index file. Increases `self.index` by one, and returns the index
-    /// at which new value was appened.
-    ///
-    /// #### Note
-    /// It is the duty of the handler of this struct to ensure index file's size does not exceed
-    /// the specified limit.
+    /// Append a new value to the index file.
+    #[inline]
     pub(super) fn append(&mut self, value: u64) -> io::Result<()> {
         self.0.seek(SeekFrom::End(0))?;
         self.0.write_u64::<BigEndian>(value)
--- src/disk/segment.rs
+++ src/disk/segment.rs
@@ -1,5 +1,38 @@
-use std::{fs::{File, OpenOptions}, io::{self, Read, Write, Seek, SeekFrom}};
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, Read, Seek, SeekFrom, Write},
+    path::Path,
+};
 
-pub struct Segment {
-    file: File,
+use bytes::{Bytes, BytesMut};
+
+pub(super) struct Segment(File);
+
+/// A wrapper around a single segment file for convenient reading of bytes. Does **not** enforce
+/// any contraints and simply does what asked. Handler should enforce the contraints.
+///
+/// #### Note
+/// It is the duty of the handler of this struct to ensure index file's size does not exceed the
+/// specified limit.
+impl Segment {
+    /// Open/create a new segment file.
+    #[inline]
+    pub(super) fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        Ok(Self(
+            OpenOptions::new()
+                .append(true)
+                .read(true)
+                .create(true)
+                .open(path)?,
+        ))
+    }
+
+    /// Reads `len` bytes from given `offset` in the file.
+    #[inline]
+    pub(super) fn read(&mut self, offset: u64, len: u64) -> io::Result<Bytes> {
+        self.0.seek(SeekFrom::Start(offset))?;
+        let mut bytes = BytesMut::with_capacity(len as usize);
+        self.0.read(&mut bytes)?;
+        Ok(bytes.into())
+    }
 }
